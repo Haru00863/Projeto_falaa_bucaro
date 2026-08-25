0734e89d1b911619daebed6cc92f688929b1755b
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2026-08-24 20:58:34 -0400

    Feat: Atualizado tela do index para que carregue a tela de login

---
+++ app/mapa.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+export default function Mapa() {
+  return (
+    <>
+      {/* tags Fragment para envolver o conteúdo do componente */}
+      <Text>Aqui vai o mapa</Text>
+    </>
+  );
+}
